9ee11835fcc78cbe1cb3119233abc5602f6b83e5
Damian Peralta <damianperaltam@gmail.com>
2019-04-14 13:32:24 +0200

    Adding babel/polyfill and core-js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import "@babel/polyfill"
+import 'core-js/es7/object'
 import React from 'react'
 import { render } from 'react-dom'
 import SimpleLoginPage from './components/LoginPage'
-
 document.addEventListener('DOMContentLoaded', () => {
   render(<SimpleLoginPage />, document.getElementById('root'))
 })
